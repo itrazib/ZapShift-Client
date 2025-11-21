2bc7999c383f0876c4e877a587b82275736ddb50
Razib Das <it.razib.mbstu@gmail.com>
2025-11-21 13:15:39 +0600

    Added Map Search Functionality

--- src/Pages/Coverage/Coverage.jsx
+++ src/Pages/Coverage/Coverage.jsx
@@ -2,16 +2,32 @@ import React from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { useLoaderData } from "react-router";
+import { useRef } from "react";
 
 const Coverage = () => {
+   const mapRef =  useRef(null)
     const serviceCenters = useLoaderData()
   const position = [23.6850, 90.3563];
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+     const location = e.target.location.value;
+
+     const district = serviceCenters.find(c => c.district.toLowerCase().includes(location.toLowerCase()))
+
+     if(district){
+        console.log(district)
+        const cord = [district.latitude,district.longitude]
+        mapRef.current.flyTo(cord,14)
+        e.target.reset()
+     }
+  }
   return (
     <div className="my-8">
       <h1 className="text-5xl font-bold">We are available in 64 districts</h1>
 
       <div className="my-5">
-        <form>
+        <form onSubmit={handleSearch}>
           <label className="input">
             <svg
               className="h-[1em] opacity-50"
@@ -29,7 +45,7 @@ const Coverage = () => {
                 <path d="m21 21-4.3-4.3"></path>
               </g>
             </svg>
-            <input type="search" required placeholder="Search" />
+            <input type="search" name="location" required placeholder="Search" />
           </label>
           <input type="submit" value="Search" className="bg-primary h-10 px-3 text-black font-semibold rounded-r-sm" />
         </form>
@@ -45,6 +61,7 @@ const Coverage = () => {
           zoom={8}
           scrollWheelZoom={false}
           className="h-[600px]"
+          ref={mapRef}
         >
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
